18198d72f5c2eb14e33a86343a34a0c2b008e7db
Dmitriy Koreshkov <puzepa@gmail.com>
2019-10-12 13:18:17 +0500

    Oimo library added. Main.js removed

---
+++ js/oimo3dmain.js
@@ -0,0 +1,9 @@
+world = new OIMO.World({
+    timestep: 1/60,
+    iterations: 8,
+    broadphase: 2, // 1 brute force, 2 sweep and prune, 3 volume tree
+    worldscale: 1, // scale full world
+    random: true,  // randomize sample
+    info: false,   // calculate statistic or not
+    gravity: [0,-9.8,0]
+});
